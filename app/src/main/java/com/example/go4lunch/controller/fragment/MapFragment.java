26380df5edee3d9914d7cf5eb0261da06d1eff2a
thomas <thomas.dutru@gmail.com>
2019-10-10 15:36:24 +0200

    ADD : Restaurant around user location are displayed on mapView

--- app/src/main/java/com/example/go4lunch/controller/fragment/MapFragment.java
+++ app/src/main/java/com/example/go4lunch/controller/fragment/MapFragment.java
@@ -1,7 +1,12 @@
 package com.example.go4lunch.controller.fragment;
 
 import android.Manifest;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,16 +14,29 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.go4lunch.R;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.PlaceLikelihood;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse;
+import com.google.android.libraries.places.api.net.PlacesClient;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
@@ -26,13 +44,16 @@ import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
     private double mLatitude;
     private double mLongitude;
 
-    private FusedLocationProviderClient fusedLocationClient;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private Place mPlace;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -42,13 +63,14 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         if (mapFragment != null)
             mapFragment.getMapAsync(this);
 
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(Objects.requireNonNull(getContext())); // Location Services
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(Objects.requireNonNull(getContext())); // Location Services
 
         return v;
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(Objects.requireNonNull(getContext()), R.raw.map_style));
         // Dexter Runtime permission for location
         Dexter.withActivity(getActivity())
                 .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
@@ -56,6 +78,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                     @Override public void onPermissionGranted(PermissionGrantedResponse response) {
                         googleMap.setMyLocationEnabled(true); // Enable MyLocation Button
                         clientLocation(googleMap); // Locate user and move the camera to his position
+                        initPlaces(googleMap);
                     }
                     @Override public void onPermissionDenied(PermissionDeniedResponse response) {
                         Toast.makeText(getContext(), "Permission denied", Toast.LENGTH_SHORT).show();
@@ -67,7 +90,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     }
 
     private void clientLocation(GoogleMap googleMap) {
-        fusedLocationClient.getLastLocation()
+        mFusedLocationClient.getLastLocation()
                 .addOnSuccessListener(Objects.requireNonNull(getActivity()), location -> {
                     if (location != null) {
                         // Logic to handle location object
@@ -84,5 +107,58 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         googleMap.setMaxZoomPreference(18);
         googleMap.setMinZoomPreference(15);
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(userPosition)); // Move the camera to user position
+    }
+
+    private void initPlaces(GoogleMap googleMap) {
+        // Initialize the SDK
+        Places.initialize(Objects.requireNonNull(getContext()), String.valueOf("AIzaSyA5AiL719efOapJiyfgf-RSBq7BN6FWDM4"));
+        // Create a new Places client instance
+        PlacesClient placesClient = Places.createClient(getContext());
+        // Use fields to define the data types to return.
+        List<Place.Field> placeFields = Arrays.asList(
+                Place.Field.LAT_LNG,
+                Place.Field.TYPES,
+                Place.Field.NAME);
+        // Use the builder to create a FindCurrentPlaceRequest.
+        FindCurrentPlaceRequest request = FindCurrentPlaceRequest.newInstance(placeFields);
+
+        Task<FindCurrentPlaceResponse> placeResponse = placesClient.findCurrentPlace(request);
+        placeResponse.addOnCompleteListener(task -> {
+            if (task.isSuccessful()){
+                FindCurrentPlaceResponse response = task.getResult();
+                assert response != null;
+
+                for (PlaceLikelihood placeLikelihood : response.getPlaceLikelihoods()) {
+                    if (Objects.requireNonNull(placeLikelihood.getPlace().getTypes()).toString().contains("RESTAURANT")) { // Display only Restaurant
+
+//                        Log.i("ABC", String.format("Place '%s' has likelihood: %f", placeLikelihood.getPlace(), placeLikelihood.getLikelihood()));
+//                        System.out.println("RESTAURANT TYPE : " +placeLikelihood.getPlace().getTypes());
+
+                        LatLng restaurant = new LatLng(Objects.requireNonNull(placeLikelihood.getPlace().getLatLng()).latitude, placeLikelihood.getPlace().getLatLng().longitude);
+
+                        googleMap.addMarker(new MarkerOptions().position(restaurant)
+                                .icon(bitmapDescriptorFromVector(getContext(), R.drawable.ic_restaurant_marker))
+                                .title(placeLikelihood.getPlace().getName()));
+                    }
+                }
+
+            } else {
+                Exception exception = task.getException();
+                if (exception instanceof ApiException) {
+                    ApiException apiException = (ApiException) exception;
+                    Log.e("ABC", "Place not found: " + apiException.getMessage());
+                }
+            }
+        });
+    }
+
+    // Convert vector image to bitmapDescriptor, here for marker icons on maps
+    private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId) {
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
+        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
     }
 }
